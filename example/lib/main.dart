2e041c61a84f389d411d95415f11f63ef620e157
Simon Joecks <simon.joecks@gmail.com>
2020-09-25 18:21:43 +0200

    Hitting the keyboard action button will cause that a tag is also created
    Also add an option to change the keyboard appearance which mainly
    works in iOS.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -51,6 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 length: values.length,
                 delimeters: [',', ' '],
                 hasAddButton: true,
+                textInputActionCreatesTag: true,
                 inputDecoration: const InputDecoration(
                   border: InputBorder.none,
                   hintText: 'Hint Text...',
