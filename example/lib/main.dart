11621e555b676e3a583c2b407f46782031edeee1
Don Coleman <coleman.drc@gmail.com>
2021-03-05 09:25:53 -0800

    Update example/lib/main.dart with default value for title
    Co-authored-by: Panu Avakul <clay864649@gmail.com>

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -40,7 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title!),
+        title: Text(widget.title ?? ''),
       ),
       body: Center(
         child: Padding(
